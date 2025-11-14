f6fdf91b20b57a376e359b64b9e37860017f6c28
AdminWalker-creator <walkerthegoat926@gmail.com>
2025-11-14 15:08:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-function openLink(url) {
-    window.open(url, "_blank");
-}
+// Open Popup
+document.getElementById("joinBtn").onclick = function () {
+    document.getElementById("popup").style.display = "flex";
+};
 
-function sendMessage() {
-    const chatInput = document.getElementById("chatInput");
-    const chatbox = document.getElementById("chatbox");
-
-    let text = chatInput.value.trim();
-    if (text === "") return;
-
-    let msg = document.createElement("div");
-    msg.className = "chat-msg";
-    msg.textContent = "You: " + text;
-
-    chatbox.appendChild(msg);
-    chatbox.scrollTop = chatbox.scrollHeight;
-
-    chatInput.value = "";
-}
+// Close Popup
+document.getElementById("closePopup").onclick = function () {
+    document.getElementById("popup").style.display = "none";
+};
